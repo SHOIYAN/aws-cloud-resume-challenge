186c6a107b46f25ea90215ce75fb86e020a7374e
SHOIYAN <shoiyan9@gmail.com>
2024-04-25 11:57:53 +0300

    initiial commit

--- website/infra/provider.tf
+++ website/infra/provider.tf
@@ -11,16 +11,3 @@ provider "aws" {
   profile ="terraform-user"
   region = "us-east-1"
 }
-terraform {
-  required_providers {
-    aws = {
-      version =">=4.9.0"
-      source = "hashicorp/aws"
-    }
-  }
-}
-
-provider "aws" {
-  profile ="terraform-user"
-  region = "us-east-1"
-}
