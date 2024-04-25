f844051c937fe01639378988f06445f980e8dd2d
SHOIYAN <shoiyan9@gmail.com>
2024-04-25 12:39:51 +0300

    initial commit

--- website/infra/main.tf
+++ website/infra/main.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "myfunc" {
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
   function_name    = "myfunc"
   role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "func.lambda_handler"
+  handler          = "myfunc.lambda_handler"
   runtime          = "python3.12"
 } 
 
@@ -66,7 +66,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 
 data "archive_file" "zip_the_python_code" {
   type        = "zip"
-  source_file = "${path.module}/lambda/func.py"
+  source_file = "${path.module}/lambda/myfunc.py"
   output_path = "${path.module}/lambda/func.zip"
 }
 
