fe6f816c574d2d61c08ed417daffa0a29e3be5d7
SHOIYAN <shoiyan9@gmail.com>
2024-04-27 13:44:26 +0300

    init

--- Back-end/infra/provider.tf
+++ Back-end/infra/provider.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5df46287e54d39d1653b034bcf01aacd62e0ac0539b7f34534d54f2b6069d1ce
-size 243
+oid sha256:23218b7b23a6e082103ad155ed3eaae89c0881287d69aaa6172c1a0b51182905
+size 177
