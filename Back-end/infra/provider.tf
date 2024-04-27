5cbbecc5a3cb99f768567a4a683a4e465372fd2d
SHOIYAN <shoiyan9@gmail.com>
2024-04-27 14:53:59 +0300

    init

--- Back-end/infra/provider.tf
+++ Back-end/infra/provider.tf
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:23218b7b23a6e082103ad155ed3eaae89c0881287d69aaa6172c1a0b51182905
-size 177
+terraform {
+  required_providers {
+    aws = {
+      
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  profile ="elliot"
+  region = "us-east-1"
+}
