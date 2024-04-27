91e8963d9f0a899a6ebf8d1177d4e025d1978f03
SHOIYAN <shoiyan9@gmail.com>
2024-04-27 21:11:21 +0300

    init

--- Back-end/infra/main.tf
+++ Back-end/infra/main.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      version =">=4.9.0"
-      source = "hashicorp/aws"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-  profile ="terraform-user"
-  
-}
-
-
-
-
 
 
 resource "aws_lambda_function" "myfunc" {
